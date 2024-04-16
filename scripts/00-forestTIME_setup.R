71c2f8d9f7f2a68407250d69d9cdb1cbbb73be30
Renata Diaz <renatadiaz@arizona.edu>
2024-04-16 13:20:07 -0700

    add creating a db...

---
+++ scripts/00-forestTIME_setup.R
@@ -0,0 +1,9 @@
+# This script installs R package dependencies and sets up the folder structure for foresTIME.
+# It is intended to be run once per computer.
+
+# Set up folder structure ####
+
+if(!dir.exists(here::here("data", "db"))) {
+  dir.create(here::here("data", "db"), recursive = T)
+}
+
