b6cdbe7fcfc4e0f83a6ae9a0b606d1ab6efe2fd2
Renata Diaz <renatadiaz@arizona.edu>
2024-04-16 14:16:50 -0700

    add all tables

--- R/create_all_tables.R
+++ R/create_all_tables.R
@@ -19,9 +19,9 @@ create_all_tables <- function(con, rawdat_dir) {
   import_tables_from_csvs(con = con,
                           csv_dir = rawdat_dir)
   add_cns_to_db(con)
-  # add_qa_flags_to_db(con)
-  # add_info_table_to_db(con)
-  # add_annual_estimates_to_db(con)
-  # add_saplings_to_db(con)
+   add_qa_flags_to_db(con)
+   add_info_table_to_db(con)
+   add_annual_estimates_to_db(con)
+   add_saplings_to_db(con)
   
 }
