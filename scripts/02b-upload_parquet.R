0fb570ea90ea3096d2aff9da578fac0b646ac27b
Renata Diaz <renatadiaz@arizona.edu>
2024-05-30 09:26:00 -0700

    Try parquets.

---
+++ scripts/02b-upload_parquet.R
@@ -0,0 +1,12 @@
+library(boxr)
+box_auth_service(token_text = Sys.getenv("BOX_TOKEN_TEXT"))
+
+# Specify the parquet file paths
+
+parquet_paths <- list.files(here::here('data', 'parquet'),
+                            full.names = T)
+
+lapply(parquet_paths,
+       box_ul,
+       dir_id =  "261941293561",
+       overwrite = TRUE)
