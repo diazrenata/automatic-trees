ac41bbf06f2d8d6a4e8419de3ad6e302afcb0975
Renata Diaz <renatadiaz@arizona.edu>
2024-08-05 11:43:20 -0700

    remove HABTYPECD1 and 3

--- scripts/03-check_db.R
+++ scripts/03-check_db.R
@@ -5,7 +5,7 @@ source(here::here("R", "create_all_tables.R"))
 
 # Specify the path to .duckdb file for database
 database_path <-
-  here::here("data", "db", "foresttime-from-parquet.duckdb")
+  here::here("data", "db", "foresttime-from-state-parquet.duckdb")
 
 if (!file.exists(database_path)) {
   warning("Database file not found.")
@@ -33,20 +33,20 @@ if (!(all(dbListTables(con) == expected_table_names))) {
   warning("Table names do not match expected table names.")
 }
 
-# # Check that sapling transitions sum to 1
-# 
-# saplings <- tbl(con, "sapling_transitions") |>
-#   collect()
-# 
-# expected_rowsums <- ifelse(saplings$PREV_live_and_skipped > 0, 1, NA)
-# 
-# if(!all.equal(rowSums(saplings[,7:13]), expected_rowsums)) {
-#   warning("Sapling proportions do not sum to 1")
-# }
-# 
-# if(!all(saplings$PREV_INVYR < saplings$INVYR)) {
-#   warning("Sapling years out of order")
-# }
+# Check that sapling transitions sum to 1
+
+saplings <- tbl(con, "sapling_transitions") |>
+  collect()
+
+expected_rowsums <- ifelse(saplings$PREV_live_and_skipped > 0, 1, NA)
+
+if(!all.equal(rowSums(saplings[,7:13]), expected_rowsums)) {
+  warning("Sapling proportions do not sum to 1")
+}
+
+if(!all(saplings$PREV_INVYR < saplings$INVYR)) {
+  warning("Sapling years out of order")
+}
 
 warning("Here is just a warning")
 
